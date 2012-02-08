1574f017f021f9c0ee2aa6ca9449207308680f77
C. Scott Ananian <cscott@cscott.net>
2012-02-08 15:20:06 -0500

    yielder: add a test case to verify all comments are preserved.

--- src/plugins/yielder/tests/comments.js
+++ src/plugins/yielder/tests/comments.js
@@ -3,17 +3,51 @@ function normal(a, b, c) {
 }
 function range(start, end, step) {
     var i;
-    step = step || 1;
-    for (i=start; i<end; i+=step) {
-        yield(i);
-    }
+/*a*/step = step || 1;/*b*/
+/*c*/for /*d*/ ( /*e*/ i/*f*/=/*g*/start/*h*/; /*hh*/
+                 /*i*/ i/*j*/ < /*k*/end/*l*/; /*m*/
+                 /*n*/ i/*o*/+=/*p*/step/*q*/) /*r*/ { /*s*/
+/*t*/  yield(i); /*u*/
+/*v*/}/*w*/
+/*x*/return;/*y*/
+}
+//m-q out of place,
+function range1(start, end, step) {
+    var i;
+/*a*/step = step || 1;/*b*/
+/*c*/i = start;/*d*/
+/*e*/for /*f*/ ( /*g*/ ; /*h*/
+                 /*i*/ i/*j*/ < /*k*/end/*l*/; /*m*/
+                 /*n*/ i/*o*/+=/*p*/step/*q*/) /*r*/ { /*s*/
+/*t*/  yield(i); /*u*/
+/*v*/}/*w*/
+/*x*/return;/*y*/
+}
+//missing f,g; m-q out of place,
+function range2(start, end, step) {
+    step = step || 1/*0*/;/*1*/
+ /*a*/ while /*b*/ ( /*c*/ start /*d*/ < /*e*/ end /*f*/) /*g*/ { /*h*/
+/*A*/ yield /*B*/ start /*C*/;/*D*/
+/*E*/ start += step;
+    /*i*/}/*j*/
+    /*k*/return;/*l*/
+}
+function range3(start, end, step) { /*a*/
+/*b*/step = step || 1;/*c*/
+/*d*/do /*e*/ { /*f*/
+        yield start;
+        start += step;
+/*g*/}/*h*/while /*i*/ ( /*j*/start < end /*k*/) /*l*/; /*m*/
+    return; /*n*/
 }
 var foo = function(a,b) {
     var x, y, z;
     x = (yield (a)) + 2;
     y = this.bar(x);
-    z = 3 * (yield y);
-    z = (yield z).foo;
+    z = 3 * (yield (y+5));
+    if (b) {
+        z = (yield z).foo;
+    }
     z = bar((yield 1), (yield 2), (yield z));
     baz(z);
     return;
@@ -24,10 +58,11 @@ function bat(arr) {
     for (i=0; i<arr.length; i++) {
         if (test.apply(arr[i], arguments)) {
             try {
-                something(null);
+                something();
                 j = yield arr[i];
-                something(j);
+                if (j) { return; }
             } catch (e) {
+                log(e);
                 delete arr[i];
             } finally {
                 baz(i);
@@ -35,4 +70,17 @@ function bat(arr) {
         }
     }
     return;
-};
+}
+function emptyblock(b) {
+/*a*/if /*b*/ (/*c*/b/*d*/)/*e*/ { /*f*/
+/*g*/}/*h*/else/*i*/{/*j*/
+/*k*/  yield /*l*/ 1 /*m*/; /*n*/
+/*o*/}/*p*/
+}
+function trytest() {
+/*a*/try/*b*/{/*c*/
+/*d*/  foo/*e*/(/*f*/)/*g*/;/*h*/
+/*i*/}/*j*/catch/*k*/(/*l*/e/*m*/)/*n*/{/*o*/
+/*p*/   yield/*q*/e/*r*/;/*s*/
+/*t*/}/*u*/
+}
